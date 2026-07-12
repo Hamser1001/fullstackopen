2329cfdcc873875604706eb8036b9b766d892206
Hamser1001 <upscale.mind50@gmail.com>
2026-07-12 16:54:14 +0100

    Complete exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,11 +2,21 @@ const Header = (props) => {
   return <h1>{props.name}</h1>;
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
     </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
   );
 };
 
@@ -22,13 +32,17 @@ function App() {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
-
   return (
     <div>
       <Header name={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
