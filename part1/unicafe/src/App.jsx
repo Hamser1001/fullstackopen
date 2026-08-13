ad0448c353f5e6b02d17b2f057d4e8e407ee5746
Hamser1001 <upscale.mind50@gmail.com>
2026-08-13 22:35:52 +0100

    Complete exercise 1.7: add statistics (total, average, positive)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,9 +11,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+
   return (
     <div>
-      <h2 style={{ marginTop: "10px" }}>give feedback</h2>
+      <h2 style={{ marginTop: "15px" }}>give feedback</h2>
       <button style={marginStyle} onClick={() => setGood(good + 1)}>
         good
       </button>
@@ -24,10 +26,15 @@ const App = () => {
         bad
       </button>
 
-      <h2 style={{ marginTop: "10px" }}>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <div>
+        <h2 style={{ marginTop: "15px" }}>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {total === 0 ? 0 : (good - bad) / total}</p>
+        <p>positive {total === 0 ? 0 : (good / total) * 100}%</p>
+      </div>
     </div>
   );
 };
