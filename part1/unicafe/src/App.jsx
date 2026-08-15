b49d0a4b94158e98693201008e5f2eea50f12c19
Hamser1001 <upscale.mind50@gmail.com>
2026-08-15 15:08:18 +0100

    Complete exercise 1.10: extract Button and StatisticLine components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,11 +5,32 @@ const marginStyle = {
   margin: "5px",
 };
 
+const StatisticLine = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  );
+};
+
 const Statistics = (props) => {
   return (
     <div>
       <h2 style={{ marginTop: "15px" }}>statistics</h2>
-      <p>good {props.good}</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.total} />
+      <StatisticLine
+        text="average"
+        value={props.total === 0 ? 0 : (props.good - props.bad) / props.total}
+      />
+      <StatisticLine
+        text="positive"
+        value={`${props.total === 0 ? 0 : (props.good / props.total) * 100} %`}
+      />
+
+      {/* <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
       <p>all {props.total}</p>
@@ -17,9 +38,17 @@ const Statistics = (props) => {
         average {props.total === 0 ? 0 : (props.good - props.bad) / props.total}
       </p>
       <p>
-        positive {props.total === 0 ? 0 : (props.good / props.total) * 100}%
-      </p>
+        positive {props.total === 0 ? 0 : (props.good / props.total) * 100} %
+      </p> */}
     </div>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <button style={marginStyle} onClick={props.onClick}>
+      {props.text}
+    </button>
   );
 };
 
@@ -34,7 +63,11 @@ const App = () => {
   return (
     <div>
       <h2 style={{ marginTop: "15px" }}>give feedback</h2>
-      <button style={marginStyle} onClick={() => setGood(good + 1)}>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      {/* <button style={marginStyle} onClick={() => setGood(good + 1)}>
         good
       </button>
       <button style={marginStyle} onClick={() => setNeutral(neutral + 1)}>
@@ -42,7 +75,8 @@ const App = () => {
       </button>
       <button style={marginStyle} onClick={() => setBad(bad + 1)}>
         bad
-      </button>
+      </button> */}
+
       {total === 0 && <p>No feedback given</p>}
       {total > 0 && (
         <Statistics good={good} neutral={neutral} bad={bad} total={total} />
