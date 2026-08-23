303df028ed68c98b5a40353170751e63ae56734e
Hamser1001 <upscale.mind50@gmail.com>
2026-08-23 22:02:12 +0100

    update: complete - exercise 2.6 - The Phonebook Step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,16 +5,32 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleInputChange = (event) => setNewName(event.target.value);
+  const handleAddBtn = (event) => {
+    event.preventDefault();
+    console.log(persons);
+    persons.map((person) => {
+      if (newName === person.name) {
+        alert(`${newName} is already added to phonebook`);
+      }
+    });
+    console.log("Clicked Button");
+  };
+
   return (
     <div>
+      <br />
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleInputChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleAddBtn}>
+            add
+          </button>
         </div>
+        <div>debug: {newName}</div>
       </form>
       <h2>Numbers</h2>
       ...
